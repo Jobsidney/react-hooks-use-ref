2de18c8929c96f847c4289ce7d5aceac622eb509
Jobsidney <jobsidney67@gmail.com>
2022-07-22 20:39:50 +0300

    DOne

--- src/components/CounterRef.js
+++ src/components/CounterRef.js
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 
 function CounterRef() {
   const count = useRef(0);
-
+let newD
   function handleClick() {
     count.current = count.current + 1;
     console.log(count.current);
